cf8951f95002218c63a623271bd60b93a6dce081
Bilalkhan5869 <52583766+Bilalkhan5869@users.noreply.github.com>
2019-07-08 03:31:19 -0700

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,21 @@
+def song():
+    print("Hello from the other side" )
+song()
+
+def year():
+    print("2015")
+year()
+
+def artist():
+    print("Adele")    
+artist()    
+    
+  
+
+def booleanret(num):
+    z = bool(num)
+    return z
+
+
+output = booleanret(1)
+print(output)
